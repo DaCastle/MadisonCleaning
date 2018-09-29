2cec2c8ab5d937973fad2643787ba93826e4f9b5
Dakota Castleberg <dakota.castleberg18@gmail.com>
2018-09-29 16:28:35 -0500

    removed counter

--- js/creative.js
+++ js/creative.js
@@ -1,10 +1,6 @@
 (function ($) {
   "use strict"; // Start of use strict
 
-  var counter = 0;
-  counter += 1;
-  console.log(counter);
-
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
