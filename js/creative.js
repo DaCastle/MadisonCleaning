a5aef1bff8ee9f221c74a20382e2b90cb934208e
Dakota Castleberg <dakota.castleberg18@gmail.com>
2018-10-04 15:22:05 -0500

    updated nav bar, only one section displayed at a time, new header image

--- js/creative.js
+++ js/creative.js
@@ -2,18 +2,18 @@
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 56)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
+  // $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+  //   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+  //     var target = $(this.hash);
+  //     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+  //     if (target.length) {
+  //       $('html, body').animate({
+  //         scrollTop: (target.offset().top - 56)
+  //       }, 1000, "easeInOutExpo");
+  //       return false;
+  //     }
+  //   }
+  // });
 
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function () {
@@ -26,67 +26,91 @@
     offset: 57
   });
 
-  // Collapse Navbar
-  var navbarCollapse = function () {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink");
-    }
-  };
-  // Collapse now if page is not at top
-  navbarCollapse();
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
+  // Collapse Navbars
+  // var navbarCollapse = function () {
+  //   if ($("#mainNav").offset().top > 100) {
+  //     $("#mainNav").addClass("navbar-shrink");
+  //   } else {
+  //     $("#mainNav").removeClass("navbar-shrink");
+  //   }
+  // };
+  // // Collapse now if page is not at top
+  // navbarCollapse();
+  // // Collapse the navbar when page is scrolled
+  // $(window).scroll(navbarCollapse);
 
-  // Scroll reveal calls
-  window.sr = ScrollReveal();
+  // // Scroll reveal calls
+  // window.sr = ScrollReveal();
 
-  sr.reveal('.sr-icon-1', {
-    delay: 200,
-    scale: 0
-  });
-  sr.reveal('.sr-icon-2', {
-    delay: 400,
-    scale: 0
-  });
-  sr.reveal('.sr-icon-3', {
-    delay: 600,
-    scale: 0
-  });
-  sr.reveal('.sr-icon-4', {
-    delay: 800,
-    scale: 0
-  });
-  sr.reveal('.sr-button', {
-    delay: 200,
-    distance: '15px',
-    origin: 'bottom',
-    scale: 0.8
-  });
-  sr.reveal('.sr-contact-1', {
-    delay: 200,
-    scale: 0
-  });
-  sr.reveal('.sr-contact-2', {
-    delay: 400,
-    scale: 0
-  });
+  // sr.reveal('.sr-icon-1', {
+  //   delay: 200,
+  //   scale: 0
+  // });
+  // sr.reveal('.sr-icon-2', {
+  //   delay: 400,
+  //   scale: 0
+  // });
+  // sr.reveal('.sr-icon-3', {
+  //   delay: 600,
+  //   scale: 0
+  // });
+  // sr.reveal('.sr-icon-4', {
+  //   delay: 800,
+  //   scale: 0
+  // });
+  // sr.reveal('.sr-button', {
+  //   delay: 200,
+  //   distance: '15px',
+  //   origin: 'bottom',
+  //   scale: 0.8
+  // });
+  // sr.reveal('.sr-contact-1', {
+  //   delay: 200,
+  //   scale: 0
+  // });
+  // sr.reveal('.sr-contact-2', {
+  //   delay: 400,
+  //   scale: 0
+  // });
 
   // Magnific popup calls
-  $('.popup-gallery').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-img-mobile',
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0, 1]
-    },
-    image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-    }
+  // $('.popup-gallery').magnificPopup({
+  //   delegate: 'a',
+  //   type: 'image',
+  //   tLoading: 'Loading image #%curr%...',
+  //   mainClass: 'mfp-img-mobile',
+  //   gallery: {
+  //     enabled: true,
+  //     navigateByImgClick: true,
+  //     preload: [0, 1]
+  //   },
+  //   image: {
+  //     tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+  //   }
+  // });
+
+  var navItems = $("#mainNav .navbar-nav>li.nav-item>a").toArray();
+
+  navItems.forEach(function (link) {
+    $(link).click(whichSlideToShow);
   });
+
+  function whichSlideToShow(link) {
+    var sectionToShow = $(link.target)[0];
+    navItems.forEach(function (link) {
+      if (link.innerHTML === sectionToShow.innerHTML) {
+        $("#" + link.innerHTML.toLocaleLowerCase()).show(1000);
+        // if ($("#" + link.innerHTML.toLocaleLowerCase()).hasClass("hidden")) {
+        //   $("#" + link.innerHTML.toLocaleLowerCase()).removeClass("hidden");
+        // }
+      }
+      else {
+        // if (!$("#" + link.innerHTML.toLocaleLowerCase()).hasClass("hidden")) {
+        $("#" + link.innerHTML.toLocaleLowerCase()).hide();
+        // }
+      }
+    });
+  }
+
 
 })(jQuery); // End of use strict
